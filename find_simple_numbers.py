ba3130caf1479565caf5eceaf88e967b61ff1dba
Oleg Sudakov <Sudakov.oleg123@yandex.ru>
2021-11-02 00:57:24 +0300

    find_simple: added an additional algorithm

--- find_simple_numbers.py
+++ find_simple_numbers.py
@@ -8,3 +8,18 @@ def find_simple(last_num):
 				simple_list.remove(j)
 	return simple_list
 		
+def diemitko(q):
+    if not q%2 or not q in find_simple(1000):
+        print("Введите нечетное, простое число")
+        return
+    simple_list = [ q*i+1 for i in range(2, 4*(q+1)) if not i%2 ]
+    result_list = []
+    for elm in simple_list:
+            a = 0
+            while(a < elm):
+                if (pow(a, elm - 1)%elm == 1) and (pow(a, (elm - 1)/q)%elm != 1):
+                        result_list.append(elm)
+                        break
+                a += 1
+                
+    return result_list
