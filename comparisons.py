f4d0d15def88e2c71f1db0797aef64b6da70b3f2
Sudakov Oleg <Sudakov.oleg123@yandex.ru>
2021-11-02 02:16:30 +0300

    Added comparisons

---
+++ comparisons.py
@@ -0,0 +1,30 @@
+from nod_e import *
+from reverse_element import *
+
+def comparisons(a, b, m):
+	if nod_e(a,m) == 1:
+		reverse_a = reverse_element(a, m)
+		a = (a * reverse_a)%m
+		b = (b * reverse_a)%m
+		if (a == 1):
+			return b
+	else:
+		nod = nod_e(a,m)
+		if b%nod == 0:
+			a /= nod
+			b /= nod
+			m /= nod
+			print(a,b,m)
+			reverse_a = reverse_element(int(a), int(m))
+			a = (a * reverse_a)%m
+			b = (b * reverse_a)%m
+			result = []
+			for i in range(0,nod):
+				result.append(b + m*i)
+			return result
+		else:
+			return -1
+			
+			
+		
+		
